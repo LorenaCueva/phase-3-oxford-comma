7395f903236662daf8229b4a825c1a5919061eb2
Lorena Cueva <lorenacueva@gmail.com>
2022-10-11 09:23:40 -0700

    commit tree e1deb87e51ca346baf4dd27715fc246eaba68b37

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+    result = ""
+    if array.length != 1
+        last = "and " + array.pop
+        array << last
+    end
+    result = array.join(" ")
+    if array.length > 2
+        result = array.join(", ")
+    end
+    result
 end
+
+puts oxford_comma(["fiddleheads", "okra", "kohlrabi"])
+puts oxford_comma(["fiddleheads", "okra"])
+puts oxford_comma(["fiddleheads"])
